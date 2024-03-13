bf38897b630ebf2287e22afe3fc22ff8b16d97bc
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-13 13:08:22 +0200

    complete HttpStatusImageDownloader class

---
+++ src/main/java/org/example/HttpStatusImageDownloader.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.*;
+import java.net.URL;
+
+public class HttpStatusImageDownloader {
+    private final HttpStatusChecker checker = new HttpStatusChecker();
+    public void downloadStatusImage(int code) {
+        try {
+            URL url = new URL(checker.getStatusImage(code));
+
+            try (InputStream in = url.openStream();
+                 OutputStream out = new FileOutputStream(code + ".jpg")) {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = in.read(buffer)) != -1) {
+                    out.write(buffer, 0, bytesRead);
+                }
+                System.out.println("Downloaded successfully");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Download failed fo status code:" + code);
+        }
+    }
+}
